a52811286c06264248e0e2d709b69eea1dfed4d8
jinnanl <jinnanl@student.unimelb.edu.au>
2018-09-21 21:03:37 +1000

    Add methods into the interface that the server may use

--- remote/IGameManager.java
+++ remote/IGameManager.java
@@ -3,12 +3,30 @@ package remote;
 import java.rmi.RemoteException;
 
 public interface IGameManager {
-	public void newClientJoined(String username) throws RemoteException;
-	
-	public void clientExit(String username) throws RemoteException;
-	
+
+	public void initiateGame(String gameState, String[] playerList) throws RemoteException;
+
+	public void clientAdded(String[] playerList) throws RemoteException;
+
+	public void roomCreated(String createplayerusername)throws RemoteException;
+
+	public boolean playerInvited() throws RemoteException;
+
+	public void invitationResponse(String invitedplayer,boolean isInvited)throws RemoteException;
+
+	public void gameStarted	(String[] playerlistGame) throws RemoteException;
+
 	public void gameTableChanged(char character, int[] index) throws RemoteException;
 	
-	public String getUserName();
-	
+	public void currentPlayer(String currentPlayer) throws RemoteException;
+
+	public boolean beginVote(int[] beginIndex, int[] endIndex) throws RemoteException;
+
+	public void voteSuccess(int score,String[]playerlist)throws RemoteException;
+
+	public void gameOver() throws RemoteException;
+
+	public void clientExited(String[] playerList) throws RemoteException;
+
+	public String getUserName() throws RemoteException;
 }
