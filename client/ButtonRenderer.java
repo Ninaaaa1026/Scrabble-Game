c18ef3e763a97f564cb27ff48b53dbb3fc102332
Chenjing-Yu <ycjing90@163.com>
2018-09-25 13:36:39 +1000

    button display

---
+++ client/ButtonRenderer.java
@@ -0,0 +1,19 @@
+package client;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class ButtonRenderer implements TableCellRenderer {
+    @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JButton button = (JButton)value;
+        if (isSelected) {
+            button.setForeground(table.getSelectionForeground());
+            button.setBackground(table.getSelectionBackground());
+        } else {
+            button.setForeground(table.getForeground());
+            button.setBackground(UIManager.getColor("Button.background"));
+        }
+        return button;
+    }
+}
